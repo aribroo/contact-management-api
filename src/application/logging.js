9fd7ca68d9a1aa34ada0ac6dccfd739397aa34e1
aribroo <rifkiari123@gmail.com>
2023-08-18 01:11:30 +0630

    Updating winston setting in logging.js

--- src/application/logging.js
+++ src/application/logging.js
@@ -2,6 +2,14 @@ import winston from 'winston';
 
 export const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.printf((info) => {
+      const { timestamp, level, message, ...args } = info;
+      const formattedMessage = `[${timestamp}] [${level}]: ${message}`;
+      return Object.keys(args).length ? `${formattedMessage} ${JSON.stringify(args, null, 2)}` : formattedMessage;
+    })
+  ),
   transports: [new winston.transports.Console({})]
 });
